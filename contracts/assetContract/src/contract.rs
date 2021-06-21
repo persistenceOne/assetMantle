7ee59e98563ab963c371292146b5e8993da9226d
Gokul S <gokuls@gmac.local>
2021-06-21 10:55:22 +0530

    contract updated

--- contracts/assetContract/src/contract.rs
+++ contracts/assetContract/src/contract.rs
@@ -1,18 +1,27 @@
 #![allow(non_snake_case)]
 
-use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
+use thiserror::Error;
 use cosmwasm_std::{
-    from_slice, log, not_found, to_vec, unauthorized,
-    Api, CanonicalAddr, CosmosMsg, Env, Extern, HandleResponse, HumanAddr,
-    InitResponse, MigrateResponse, Querier, QueryResponse, StdResult, Storage,
+    attr, to_vec, from_slice, Addr, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, Storage, StdError,
 };
+use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+use schemars::JsonSchema;
+
+pub static CONFIG_KEY: &[u8] = b"config";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InitMsg {
-    pub verifier: HumanAddr,
-    pub beneficiary: HumanAddr,
+pub struct State {
+    pub arbiter: Addr,
+    pub recipient: Addr,
+    pub source: Addr,
+}
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub arbiter: String,
+    pub recipient: String,
 }
 
 /// MigrateMsg allows a priviledged contract administrator to run
@@ -24,88 +33,104 @@ pub struct InitMsg {
 /// by blockchain logic (in the future by blockchain governance)
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct MigrateMsg {
-    pub verifier: HumanAddr,
+    pub arbiter: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {}
 
-pub fn migrate<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    _env: Env,
-    msg: MigrateMsg,
-) -> StdResult<MigrateResponse> {
-    let data = deps
-        .storage
-        .get(CONFIG_KEY)
-        .ok_or_else(|| not_found("State"))?;
-    let mut config: State = from_slice(&data)?;
-    config.verifier = deps.api.canonical_address(&msg.verifier)?;
-    deps.storage.set(CONFIG_KEY, &to_vec(&config)?);
-    Ok(MigrateResponse::default())
-}
-
-pub fn query<S: Storage, A: Api, Q: Querier>(
-    _deps: &Extern<S, A, Q>,
-    msg: QueryMsg,
-) -> StdResult<QueryResponse> {
-    match msg {}
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub verifier: CanonicalAddr,
-    pub beneficiary: CanonicalAddr,
-    pub funder: CanonicalAddr,
-}
 
 // failure modes to help test wasmd, based on this comment
 // https://github.com/cosmwasm/wasmd/issues/8#issuecomment-576146751
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum HandleMsg {
+pub enum ExecuteMsg {
     AssetMint { properties: String },
 }
 
-pub static CONFIG_KEY: &[u8] = b"config";
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
 
-pub fn init<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-    msg: InitMsg,
-) -> StdResult<InitResponse> {
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+}
+
+pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
+    singleton(storage, CONFIG_KEY)
+}
+
+pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<State> {
+    singleton_read(storage, CONFIG_KEY)
+}
+
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    deps.api.debug("here we go 🚀");
+
     deps.storage.set(
         CONFIG_KEY,
         &to_vec(&State {
-            verifier: deps.api.canonical_address(&msg.verifier)?,
-            beneficiary: deps.api.canonical_address(&msg.beneficiary)?,
-            funder: env.message.sender,
+            arbiter: deps.api.addr_validate(&msg.arbiter)?,
+            recipient: deps.api.addr_validate(&msg.recipient)?,
+            source: info.sender,
         })?,
     );
 
-    // This adds some unrelated data and log for testing purposes
-    Ok(InitResponse {
-        messages: vec![],
-        log: vec![log("Let the", "hacking begin")],
-        ..InitResponse::default()
-    })
+    // This adds some unrelated event attribute for testing purposes
+    let mut resp = Response::new();
+    resp.add_attribute("Let the", "hacking begin");
+    Ok(resp)
 }
 
-pub fn handle<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
+
+pub fn migrate(
+    deps: DepsMut,
     env: Env,
-    msg: HandleMsg,
-) -> StdResult<HandleResponse<PersistenceSDK>> {
+    msg: MigrateMsg,
+) -> Result<Response, ContractError> {
+    let data = deps
+        .storage
+        .get(CONFIG_KEY)
+        .ok_or_else(|| StdError::not_found("State"))?;
+    let mut config: State = from_slice(&data)?;
+    config.arbiter = deps.api.addr_validate(&msg.arbiter)?;
+    deps.storage.set(CONFIG_KEY, &to_vec(&config)?);
+
+    Ok(Response::default())
+
+}
+
+pub fn query(
+    deps: Deps,
+    env: Env,
+    msg: QueryMsg,
+) -> Result<Response, ContractError> {
+    match msg {}
+}
+
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response<PersistenceSDK>, ContractError> {
     match msg {
-        HandleMsg::AssetMint { properties } => do_asset_mint(deps, env, properties),
+        ExecuteMsg::AssetMint { properties } => do_asset_mint(deps, env, properties, info),
     }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct AssetMintRaw {
-    from: HumanAddr,
+    from: String,
     chainID: String,
     maintainersID: String,
     classificationID: String,
@@ -129,42 +154,40 @@ impl From<PersistenceSDK> for CosmosMsg<PersistenceSDK> {
     }
 }
 
-fn do_asset_mint<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
+fn do_asset_mint(
+    deps: DepsMut,
     env: Env,
     properties: String,
-) -> StdResult<HandleResponse<PersistenceSDK>> {
-    let data = deps
-        .storage
-        .get(CONFIG_KEY)
-        .ok_or_else(|| not_found("State"))?;
-    let state: State = from_slice(&data)?;
-
-    if env.message.sender == state.verifier {
-        let from_addr = deps.api.human_address(&env.contract.address)?;
-
-        // can add all the parameters as input params
-        let mintMsg = AssetMintRaw {
-            from: deps.api.human_address(&env.message.sender)?,
-            chainID: "".to_owned(),
-            maintainersID: "".to_owned(),
-            classificationID: "".to_owned(),
-            asset_properties: properties,
-            lock: -1,
-            burn: -1,
-        };
-
-        let res = HandleResponse {
-            log: vec![log("action", "asset_mint"), log("destination", &from_addr)],
-            messages: vec![PersistenceSDK {
-                msgtype: "assets/mint".to_string(),
-                raw: mintMsg,
-            }
-            .into()],
-            data: None,
-        };
-        Ok(res)
-    } else {
-        Err(unauthorized())
-    }
+    info: MessageInfo,
+) -> Result<Response<PersistenceSDK>, ContractError> {
+    let state = config_read(deps.storage).load()?;
+        if info.sender == state.arbiter {
+            let from_addr = deps.api.addr_validate(&env.contract.address.to_string())?;
+    
+            // can add all the parameters as input params
+            let mintMsg = AssetMintRaw {
+                from: deps.api.addr_validate(&info.sender.to_string())?.to_string(),
+                chainID: "".to_owned(),
+                maintainersID: "".to_owned(),
+                classificationID: "".to_owned(),
+                asset_properties: properties,
+                lock: -1,
+                burn: -1,
+            };
+    
+            let res = Response {
+                submessages: vec![],
+                messages: vec![PersistenceSDK {
+                    msgtype: "assets/mint".to_string(),
+                    raw: mintMsg,
+                }
+                .into()],
+                attributes: vec![attr("action", "asset_mint"), attr("destination", &from_addr)],
+                data: None,
+            };
+            Ok(res)
+        } else {
+            Err(ContractError::Unauthorized {})
+        }
+    
 }
